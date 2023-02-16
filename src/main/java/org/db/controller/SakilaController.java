a6f850b40ac0bccc08deacf905c0023276d161a4
cjp4 <carlos.jorge@inetum.com>
2023-02-15 21:31:12 -0500

    correcion por sonar

--- src/main/java/org/db/controller/SakilaController.java
+++ src/main/java/org/db/controller/SakilaController.java
@@ -7,7 +7,6 @@ import org.db.model.Film;
 import org.db.service.SakilaService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
